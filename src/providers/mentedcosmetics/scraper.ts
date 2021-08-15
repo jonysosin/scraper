71f605ced8e508cc08eb0b9e4bcbc439474210c4
Luka Brizzi <lukabrizzi@Air-de-Luka.fibertel.com.ar>
2021-08-15 13:54:30 -0300

    [mentedcosmetics] Add correction to scraper

--- src/providers/mentedcosmetics/scraper.ts
+++ src/providers/mentedcosmetics/scraper.ts
@@ -3,6 +3,7 @@ import { getProductOptions } from '../shopify/helpers'
 import shopifyScraper, { TShopifyExtraData } from '../shopify/scraper'
 import parseHtmlTextContent from '../../providerHelpers/parseHtmlTextContent'
 import { DESCRIPTION_PLACEMENT } from '../../interfaces/outputProduct'
+import { TMediaImage } from '../shopify/types'
 
 export default shopifyScraper(
   {
@@ -33,12 +34,33 @@ export default shopifyScraper(
     },
     variantFn: async (_request, page, product, providerProduct, providerVariant, extraData) => {
       /**
+       * Normalize the brand
+       */
+       if (product.brand && ['Mented'].includes(product.brand)) {
+        product.brand = 'Mented Cosmetics'
+      }
+      /**
        * Get the list of options for the variants of this provider
        * (5) ["Title", "Color", "Denominations", "Shade", "Combination"]
        */
       const optionsObj = getProductOptions(providerProduct, providerVariant)
       if (optionsObj.Color || optionsObj.Shade) {
         product.color = optionsObj.Color || optionsObj.Shade
+      }
+
+      /**
+       * Replace all the product images with the ones related by color (only if there're matches)
+       */
+       if (product.color) {
+        const images = (providerProduct.media as TMediaImage[])
+          .filter(e => e.alt !== null)
+          .filter(e => e.alt === product.color)
+          .map(e => e?.src)
+          .filter(e => e !== '')
+
+        if (images.length) {
+          product.images = images
+        }
       }
 
       // Remove the first element of the array, as the additional section captured by the generic shopify scraper is not correct in this case
@@ -56,6 +78,19 @@ export default shopifyScraper(
       if (descriptionSection) {
         product.description = descriptionSection
       }
+
+      /**
+       * Get the video by clicking preview images (if it exists)
+       */
+       const videos = await page.evaluate(async () => {
+        return Array.from(document.querySelectorAll('.howtouse-container img')).map(
+          // @ts-ignore
+          e => `https://www.youtube.com/watch?v=${e.dataset?.embed}`,
+        )
+      })
+      videos?.forEach(element => {
+        product.videos.push(element)
+      })
     },
   },
   {},
