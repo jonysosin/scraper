ec808f9df22d38fd21747423e512ea225edf9061
Luka Brizzi <lukabrizzi@MacBook-Air-de-Luka.local>
2021-08-15 03:30:51 -0300

    [mentedcosmetics] Add correction to scraper

--- src/providers/mentedcosmetics/scraper.ts
+++ src/providers/mentedcosmetics/scraper.ts
@@ -1,10 +1,13 @@
+import { getSelectorOuterHtml } from '../../providerHelpers/getSelectorOuterHtml'
 import { getProductOptions } from '../shopify/helpers'
 import shopifyScraper, { TShopifyExtraData } from '../shopify/scraper'
+import parseHtmlTextContent from '../../providerHelpers/parseHtmlTextContent'
+import { DESCRIPTION_PLACEMENT } from '../../interfaces/outputProduct'
 
 export default shopifyScraper(
   {
     productFn: async (_request, page) => {
-      const extraData: TShopifyExtraData = {}
+      const extraData: TShopifyExtraData = { additionalSections: [] }
       /**
        * Get the breadcrumbs
        */
@@ -14,9 +17,21 @@ export default shopifyScraper(
           .filter(e => e !== '')
       })
 
+       /**
+       * Add the "Products Details" section
+       */
+      const productDetails = await getSelectorOuterHtml(page, '.Product__Details')
+      if (productDetails) {
+        extraData.additionalSections?.push({
+          name: 'Products Details',
+          content: productDetails,
+          description_placement: DESCRIPTION_PLACEMENT.DISTANT,
+        })
+      }
+
       return extraData
     },
-    variantFn: async (_request, _page, product, providerProduct, providerVariant) => {
+    variantFn: async (_request, page, product, providerProduct, providerVariant, extraData) => {
       /**
        * Get the list of options for the variants of this provider
        * (5) ["Title", "Color", "Denominations", "Shade", "Combination"]
@@ -24,6 +39,22 @@ export default shopifyScraper(
       const optionsObj = getProductOptions(providerProduct, providerVariant)
       if (optionsObj.Color || optionsObj.Shade) {
         product.color = optionsObj.Color || optionsObj.Shade
+      }
+
+      // Remove the first element of the array, as the additional section captured by the generic shopify scraper is not correct in this case
+      product.additionalSections.shift()
+       /**
+       * Replace the original description with the one displayed in the website
+       */
+      await page.evaluate(() => {
+        document.querySelector('.s-product__dropdown-header')?.remove()
+      })
+      const descriptionSection = await page.evaluate(() => {
+        return document.querySelector('.ProductMeta__Description')?.textContent?.trim()
+      })
+
+      if (descriptionSection) {
+        product.description = descriptionSection
       }
     },
   },
