7f968fc3922feb154cb591c49714f6a8fc49e51e
Fran Espeche <franespeche@ScaleC0T2RHMD6P.fibertel.com.ar>
2021-08-15 15:41:28 -0300

    fixed patmcgrath

--- src/providers/patmcgrath/scraper.ts
+++ src/providers/patmcgrath/scraper.ts
@@ -1,43 +1,42 @@
+import { TMediaImage } from '../../providers/shopify/types'
 import { DESCRIPTION_PLACEMENT } from '../../interfaces/outputProduct'
 import { getProductOptions } from '../shopify/helpers'
 import shopifyScraper, { TShopifyExtraData } from '../shopify/scraper'
 
 export default shopifyScraper(
   {
-    productFn: async (_request, page) => {
-      const extraData: TShopifyExtraData = {}
-
+    variantFn: async (_request, page, product, providerProduct, providerVariant) => {
       /**
-       * Get additional descriptions and information
+       * Get additional descriptions and information (by variant)
        */
-      extraData.additionalSections = await page.evaluate(DESCRIPTION_PLACEMENT => {
-        const keysSelector = '#accordion > div > h3'
-        const valuesSelector = '#accordion > div > div'
+      const variantTitle = providerVariant.title
+      product.additionalSections = await page.evaluate(
+        (DESCRIPTION_PLACEMENT, variantTitle) => {
+          return Array.from(
+            document.querySelectorAll(`#accordion > [data-variant="${variantTitle}"]`),
+          )
+            .map(e => {
+              const name = e?.textContent?.trim() || ``
+              return {
+                name: e.querySelector('h3')?.textContent?.trim() || '',
+                content:
+                  e.querySelector('h3')?.parentElement?.querySelector('div')?.outerHTML?.trim() ||
+                  '',
+                description_placement:
+                  name === 'DESCRIPTION'
+                    ? DESCRIPTION_PLACEMENT.MAIN
+                    : DESCRIPTION_PLACEMENT.ADJACENT,
+              }
+            })
+            .filter(e => !['reviews'].includes(e.name.toLowerCase()))
+        },
+        DESCRIPTION_PLACEMENT,
+        variantTitle,
+      )
 
-        // Get a list of titles
-        const keys = Array.from(document.querySelectorAll(keysSelector))
-
-        // Get a list of content for the titles above
-        const values = Array.from(document.querySelectorAll(valuesSelector))
-
-        // Join the two arrays
-        return values.map((value, i) => {
-          const name = keys[i]?.textContent?.trim() || `key_${i}`
-          return {
-            name,
-            content: value?.outerHTML?.trim() || '',
-            description_placement:
-              name === 'DESCRIPTION' ? DESCRIPTION_PLACEMENT.MAIN : DESCRIPTION_PLACEMENT.ADJACENT,
-          }
-        })
-      }, DESCRIPTION_PLACEMENT)
-
-      return extraData
-    },
-    variantFn: async (_request, _page, product, providerProduct, providerVariant) => {
       /**
        * Get the list of options for the variants of this provider
-       * (5) ["Title", "Color", "Style", "Amount", "Size"]
+       * (5) ["Title", "Color", "Style", "Amount", "Size"]
        */
       const optionsObj = getProductOptions(providerProduct, providerVariant)
       if (optionsObj.Color) {
@@ -48,14 +47,27 @@ export default shopifyScraper(
       }
 
       /**
-       * Remove the first element of the array, as the additional section captured by the generic shopify scraper is not correct in this case
-       */
-      product.additionalSections.shift()
-
-      /**
        * The product title is in the product level, not the variant
        */
-      product.title = providerProduct.title
+      product.title = providerProduct.title.split(': ')[0]
+
+      /**
+       * Get additional descriptions and information
+       */
+
+      /**
+       * Replace all the product images with the ones related by color (only if there're matches)
+       */
+      if (product.color) {
+        const images = (providerProduct.media as TMediaImage[])
+          .filter(e => e.alt?.split('||')[0] === product.color)
+          .map(e => e?.src)
+          .filter(e => e !== '')
+
+        if (images.length) {
+          product.images = images
+        }
+      }
     },
   },
   {},
