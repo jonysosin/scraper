df2f6f7284a34a22f3915811b45eeb6867727bcc
Luka Brizzi <lukabrizzi@MacBook-Air-de-Luka.local>
2021-08-17 20:34:50 -0300

    Add correction to scraper

--- src/providers/shopimpressions/scraper.ts
+++ src/providers/shopimpressions/scraper.ts
@@ -7,41 +7,46 @@ export default shopifyScraper(
   {
     productFn: async (_request, page) => {
       const extraData: TShopifyExtraData = {}
-
       /**
        * Get additional descriptions and information
        */
       extraData.additionalSections = await page.evaluate(DESCRIPTION_PLACEMENT => {
-        // Get a list of titles
-        const keys = Array.from(document.querySelectorAll('.itemDescriptionWrapper .text-center'))
-
         // Get a list of content for the titles above
         const values = Array.from(
-          document.querySelectorAll('.itemDescriptionWrapper ul'),
+          Array.from(document.querySelectorAll('.itemDescriptionWrapper ul')).map(e => e.innerHTML?.trim() || '').filter(e => e !== ''),
         )
         // Join the two arrays
         const sections = values.map((value, i) => {
           return {
-            name: keys[i].textContent?.trim() || `key_${i}`,
-            content: value.innerHTML?.trim() || '',
-            description_placement: DESCRIPTION_PLACEMENT.MAIN,
+            name: 'Additional data',
+            content: value || '',
+            description_placement: DESCRIPTION_PLACEMENT.ADJACENT,
           }
         })
 
         return sections
       }, DESCRIPTION_PLACEMENT)
 
-           /**
-       * Get Size Chart HTML
-       */
-            const sizeChartTitle = await getSelectorOuterHtml(page, '.itemDescSizeStats:first-child')
-            const sizeChartContent = await getSelectorOuterHtml(page, '.modalModelFitBody:first-child')
-            const modelStatsTitle = await getSelectorOuterHtml(page, '.itemDescSizeStats:first-child')
-            const modelStatsContent = await getSelectorOuterHtml(page, '.modalModelFitBody:first-child')
-            const sizeChart = sizeChartTitle.concat(sizeChartContent)
-            const modelStats = modelStatsTitle.concat(modelStatsContent)
+      /**
+      * This site differs from the others and has a particular description included in the HTML (not the JSON)
+      */
+       const description = await page.evaluate(() => {
+        return document.querySelector('#description')?.outerHTML.trim()
+      })
+      if (description) {
+        extraData.additionalSections?.push({
+          name: 'Description',
+          content: description,
+          description_placement: DESCRIPTION_PLACEMENT.MAIN,
+        })
+      }
 
-            extraData.sizeChartHtml = sizeChart.concat(modelStats)
+      /**
+      * Get Size Chart HTML
+      */
+       extraData.sizeChartHtml = await page.evaluate(() => {
+         return document.querySelector('.sizingChart')?.outerHTML?.replace(/\s*/g, '')
+       })
 
       return extraData
     },
@@ -60,8 +65,11 @@ export default shopifyScraper(
     if (optionsObj.Color) {
       product.color = optionsObj.Color
     }
+    /**
+     * Cut the first element of array
+     */
+    product.additionalSections.shift()
 
-     
     },
   },
   {},
