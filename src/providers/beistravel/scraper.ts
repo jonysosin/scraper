f7e05908dab69a22f2ecfad662d1e00eeb0ba59f
fran <fespeche@gmail.com>
2021-08-17 20:35:47 -0300

    [BeisTravel] product color, key_value and options fix

--- src/providers/beistravel/scraper.ts
+++ src/providers/beistravel/scraper.ts
@@ -11,13 +11,18 @@ export default shopifyScraper(
       /**
        * Get additional descriptions and information
        */
-      const aditionalSection = await page.evaluate(DESCRIPTION_PLACEMENT => {
+      const additionalSection = await page.evaluate(DESCRIPTION_PLACEMENT => {
         // Get a list of titles
-        const arrayData = Array.from(document.querySelectorAll(".productDesc table tr:not(.open):not([style='display: block;'])"))
-        const keys:any = []
-        const values:any = []
-        for (let i = 0; i< arrayData.length ; i++){
-          i % 2 == 0? keys.push(arrayData[i]) : values.push(arrayData[i])
+        const arrayData = Array.from(
+          document.querySelectorAll(
+            ".productDesc table tr:not(.open):not([style='display: block;'])",
+          ),
+        )
+
+        const keys: any = []
+        const values: any = []
+        for (let i = 0; i < arrayData.length; i++) {
+          i % 2 == 0 ? keys.push(arrayData[i]) : values.push(arrayData[i])
         }
 
         // Join the two arrays
@@ -34,10 +39,10 @@ export default shopifyScraper(
 
       const productStory = await page.evaluate(DESCRIPTION_PLACEMENT => {
         // Get a list of titles
-        const values = Array.from(document.querySelectorAll(".product-story"))
+        const values = Array.from(document.querySelectorAll('.product-story'))
 
         // Join the two arrays
-        const sections = values.map((value) => {
+        const sections = values.map(value => {
           return {
             name: `Product Story`,
             content: value?.outerHTML?.trim() || '',
@@ -53,33 +58,40 @@ export default shopifyScraper(
         const values = Array.from(document.querySelectorAll('.js-product-description table tr'))[1]
 
         // Join the two arrays
-        const sections = [{
+        const sections = [
+          {
             name: `Description`,
             content: values.outerHTML?.trim() || '',
             description_placement: DESCRIPTION_PLACEMENT.MAIN,
-          }
+          },
         ]
 
         return sections
       }, DESCRIPTION_PLACEMENT)
 
-      extraData.additionalSections =[...mainDescription, ...aditionalSection, ...productStory ]
-
+      extraData.additionalSections = [...mainDescription, ...additionalSection, ...productStory]
       /**
        * Get Size Chart HTML
        */
-      extraData.sizeChartHtml = await getSelectorOuterHtml(page, "div.human-mode")
+      extraData.sizeChartHtml = await getSelectorOuterHtml(page, 'div.human-mode')
 
       return extraData
     },
-    variantFn: async (_request, _page, product, providerProduct, providerVariant) => {
+    variantFn: async (_request, page, product, providerProduct, providerVariant) => {
       /**
-       * (2) ["Title", "Color"]
+       * Get the color of the product
        */
-      const optionsObj = getProductOptions(providerProduct, providerVariant)
-      if (optionsObj.Color) {
-        product.color = optionsObj.Color
+      const color = await page.evaluate(() => {
+        return document.querySelector('.product__current-color')?.textContent?.trim() || ''
+      })
+
+      if (color) {
+        product.color = color
       }
+      /**
+       * Adding product selected color into options
+       */
+      product.options = { ...product.options, Color: color }
 
       /**
        * Cut the first element from array
