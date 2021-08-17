f49753d9af8a4cd84304b8ae71e99df6d18c6dfa
Mathias Efron <mathias.efron@contractors.scale.com>
2021-08-17 20:32:45 -0300

    [altardstate] fix base url

--- src/providers/altardstate/scraper.ts
+++ src/providers/altardstate/scraper.ts
@@ -4,15 +4,14 @@ import { IDescriptionSection, DESCRIPTION_PLACEMENT } from '../../interfaces/out
 import IScraper from '../../interfaces/scraper'
 import { getSelectorOuterHtml } from '../../providerHelpers/getSelectorOuterHtml'
 import screenPage from '../../utils/capture'
-import type { Page } from 'puppeteer'
+import { Page } from 'puppeteer'
 import { TProviderData } from './types'
 import { omit } from 'lodash'
 import ommitKeys from './omitKeys'
 import parseHtmlTextContent from '../../providerHelpers/parseHtmlTextContent'
-
+import { last } from 'lodash'
 
 export const getProductJson = async (page: Page, productUrl: string) => {
-
   await page.goto(productUrl)
 
   const productData = await page.evaluate(() => {
@@ -32,37 +31,44 @@ export const getProductJson = async (page: Page, productUrl: string) => {
 
   if (!productData) {
     // TODO: Improve error handling for this cases
-    throw new Error('Could not find product information for this url')
+    throw new Error(`Could not find product json for ${productUrl}`)
   }
 
   return productData
 }
 
 const scraper: IScraper = async (request, page) => {
+  const { protocol, resource, pathname } = parseUrl(request.pageUrl)
 
-  const { protocol, resource, pathname, query: { pid } } = parseUrl(request.pageUrl)
-
-  const baseProductUrl = `${protocol}://${resource}${pathname}?pid=${pid}`
+  const pid = last(pathname.split('/'))?.split('.html')[0] || ''
+  const baseProductUrl = `https://www.altardstate.com/on/demandware.store/Sites-altardstate-Site/default/Product-Variation?pid=${pid}`
+  console.log(baseProductUrl)
   const baseProductData = await getProductJson(page, baseProductUrl)
 
   // Website screenshot
-  // console.log(`${protocol}://${resource}${baseProductData.product.selectedProductUrl.split('?')[0]}`)
-  await page.goto(`${protocol}://${resource}${baseProductData.product.selectedProductUrl.split('?')[0]}`)
+  await page.goto(request.pageUrl)
   const screenshot = await screenPage(page)
 
   // Additional sections
-  const additionalSections = await page.evaluate((DESCRIPTION_PLACEMENT) => {
+  const additionalSections = await page.evaluate(DESCRIPTION_PLACEMENT => {
     const sections: IDescriptionSection[] = []
     document.querySelectorAll('.pdp-description-details .accordion-wrapper').forEach(element => {
-      const name = (element.querySelector('.pdp-link-desc-detail')?.textContent || '').replaceAll('\n', '').trim()
-      const content = (element.querySelector('.more-info-body')?.outerHTML || '').replaceAll('\n', '').trim()
+      const name = (element.querySelector('.pdp-link-desc-detail')?.textContent || '')
+        .replaceAll('\n', '')
+        .trim()
+      const content = (element.querySelector('.more-info-body')?.outerHTML || '')
+        .replaceAll('\n', '')
+        .trim()
       sections.push({ name, content, description_placement: DESCRIPTION_PLACEMENT.ADJACENT })
     })
     return sections
   }, DESCRIPTION_PLACEMENT)
 
   // Size chart html
-  const sizeChartHtml = await getSelectorOuterHtml(page, '#openSizeGuideModal > div > div > div.modal-body > div')
+  const sizeChartHtml = await getSelectorOuterHtml(
+    page,
+    '#openSizeGuideModal > div > div > div.modal-body > div',
+  )
 
   // Breadcrumbs
   const breadcrumbs = baseProductData.variantGTMJson[0].category.split('/')
@@ -89,8 +95,12 @@ const scraper: IScraper = async (request, page) => {
       product.images = []
     }
 
-    const variantSize = productData.product.variationAttributes.find(x => x.attributeId === 'size')?.values.find(x => !!x.selected)
-    const variantColor = productData.product.variationAttributes.find(x => x.attributeId === 'color')?.values.find(x => !!x.selected)
+    const variantSize = productData.product.variationAttributes
+      .find(x => x.attributeId === 'size')
+      ?.values.find(x => !!x.selected)
+    const variantColor = productData.product.variationAttributes
+      .find(x => x.attributeId === 'color')
+      ?.values.find(x => !!x.selected)
 
     product.size = variantSize?.displayValue
     product.color = variantColor?.displayValue
@@ -98,10 +108,11 @@ const scraper: IScraper = async (request, page) => {
     product.realPrice = productData.product.price.sales?.value
     product.higherPrice = productData.product.price.list?.value
     product.currency = productData.product.price.sales.currency
-    product.availability = productData.product.available && variantSize?.selectable && variantColor?.selectable
+    product.availability =
+      productData.product.available && variantSize?.selectable && variantColor?.selectable
     product.description = parseHtmlTextContent(productData.product.longDescription)
     product.bullets = Object.values(omit(productData.product, ommitKeys)) as string[]
-    product.keyValuePairs = omit(productData.product, ommitKeys) as { string: string };
+    product.keyValuePairs = omit(productData.product, ommitKeys) as { string: string }
     product.breadcrumbs = breadcrumbs
     product.sizeChartHtml = sizeChartHtml
     product.addAdditionalSections(additionalSections)
@@ -114,8 +125,14 @@ const scraper: IScraper = async (request, page) => {
 
 export default scraper
 
-const getProductVariantUrls = (baseProductData: TProviderData, baseProductUrl: string, productId: string) => {
-  const variants = baseProductData.product.variationAttributes.map(({ attributeId, values }) => values.map(x => `${attributeId}:${x.value}`));
+const getProductVariantUrls = (
+  baseProductData: TProviderData,
+  baseProductUrl: string,
+  productId: string,
+) => {
+  const variants = baseProductData.product.variationAttributes.map(({ attributeId, values }) =>
+    values.map(x => `${attributeId}:${x.value}`),
+  )
   const combinedVariants = combine(variants)
 
   // console.log('combinedVariants', combinedVariants)
@@ -129,13 +146,12 @@ const getProductVariantUrls = (baseProductData: TProviderData, baseProductUrl: s
     })
     url += '&quantity=1'
     variantsData.push({ id: `${productId}-${variant}`, url })
-  });
+  })
 
-  return variantsData;
+  return variantsData
 }
 
 const combine = (options: string[][], splitter = '-'): string[] => {
-
   const [head, ...[headTail, ...tailTail]] = options
 
   if (!headTail) return head
